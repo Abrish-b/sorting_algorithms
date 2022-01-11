8757c42308cbbc729baa721f13b5f343f39b499a
Abrham Bunaro <abrishbunaro@gmail.com>
2022-01-11 10:31:09 -0800

    quick fix

--- deck.h
+++ deck.h
@@ -1,29 +1,32 @@
-#ifndef DECK_H
-#define DECK_H
+#ifndef _DECK_H_
+#define _DECK_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-
-/* the structs they gave us */
+/**
+ * enum kind_e - suit of the card
+ *
+ * @SPADE: 0, or spades
+ * @HEART: 1, or hearts
+ * @CLUB: 2, or clubs
+ * @DIAMOND: 3, or diamonds
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
- * struct card_s - Playing card
- *
+ * struct card_s - card representation
  * @value: Value of the card
  * From "Ace" to "King"
  * @kind: Kind of the card
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -35,10 +38,11 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
-endif
+
+#endif /* _DECK_H_ */
